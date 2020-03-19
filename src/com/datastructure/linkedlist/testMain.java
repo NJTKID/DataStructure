3651d5d0b47b9c253f1ac9f36e5b0aecd8ea127b
NJTKID <335600324@qq.com>
2020-03-20 02:47:56 +0800

    链表

---
+++ src/com/datastructure/linkedlist/testMain.java
@@ -0,0 +1,40 @@
+package com.datastructure.linkedlist;
+
+import java.util.Random;
+
+import com.datastructure.stack.ArrayStack;
+import com.datastructure.stack.Stack;
+
+public class testMain {
+
+	private static double testStack(Stack<Integer> stack,int opCount){
+		long startTime = System.nanoTime();
+		
+		Random random = new Random();
+		for(int i = 0; i < opCount ; i++)
+			stack.push(random.nextInt(Integer.MAX_VALUE));
+		
+		for(int i = 0; i < opCount ; i++)
+			stack.pop();
+		
+		long endTime = System.nanoTime();
+		
+		return (endTime - startTime) / 1000000000.0;
+		
+	}
+	
+	public static void main(String[] args){
+		int opCount = 100000;
+		
+		ArrayStack<Integer> arrayStack = new ArrayStack<>();
+		double time1 = testStack(arrayStack,opCount);
+		System.out.println("ArrayStack，time" + time1 + "s");
+		
+		LinkedListStack<Integer> linkedListStack = new LinkedListStack<>();
+		double time2 = testStack(linkedListStack,opCount);
+		System.out.println("LinkedListStack，time" + time2 + "s");
+		
+		//其实这个时间复杂度比较复杂，因为LinkedListStack中包含更多的new操作
+	}
+
+}
