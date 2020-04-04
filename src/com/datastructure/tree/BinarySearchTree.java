6f81a2cf0421a43d9617bd6924a08ca87de4eb6e
NJTKID <335600324@qq.com>
2020-04-04 17:04:39 +0800

    二叉搜索树的增删查操作

--- src/com/datastructure/tree/BinarySearchTree.java
+++ src/com/datastructure/tree/BinarySearchTree.java
@@ -1,5 +1,8 @@
 package com.datastructure.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 //二分搜索树：节点的值大于其所有左子树节点的值，且小于其所有右子树节点的值的二叉树
 //二叉树不是支持所有类型，而是支持可比较的类型
@@ -34,17 +37,19 @@ public class BinarySearchTree <E extends Comparable<E>>{
 	
 	//项二分搜索树中添加新的元素e
 	public void add(E e){
-		if(root == null){
+		/*if(root == null){
 			root = new Node(e);
 			size ++;
 		}else
-			add(root,e);
+			add(root,e);*/
+		root = add(root,e);
 	}
 	
 	//向以node为根的二分搜索树中插入元素e，递归算法
-	private void add(Node node,E e){
+	//返回插入新节点后二分搜索树的根
+	private Node add(Node node,E e){
 		
-		if(e.equals(node.e))
+		/*if(e.equals(node.e))
 			return;
 		else if(e.compareTo(node.e) < 0 && node.left == null){
 			node.left = new Node(e);
@@ -59,6 +64,260 @@ public class BinarySearchTree <E extends Comparable<E>>{
 		if(e.compareTo(node.e) < 0)
 			add(node.left , e);
 		else	//e.compareTo(node.e) > 0
-			add(node.right , e);
+			add(node.right , e);*/
+		
+		if(node == null){
+			size++;
+			return new Node(e);
+		}
+		
+		if(e.compareTo(node.e) < 0){
+			node.left = add(node.left,e);
+		}else if(e.compareTo(node.e) > 0){
+			node.right = add(node.right,e);
+		}
+		
+		return node;
+	}
+	
+	//看二分搜索树中是否包含元素e
+	public boolean contains(E e){
+		return contains(root,e);
+	}
+	
+	//看以node为根的二分搜索树中是否包含元素e,递归算法
+	private boolean contains(Node node,E e){
+		if(node == null)
+			return false;
+		
+		if(e.compareTo(node.e) == 0)
+			return true;
+		else if(e.compareTo(node.e) < 0)
+			return contains(node.left,e);
+		else // e.compareTo(node.e) > 0
+			return contains(node.right,e);
+	}
+	
+	//二分搜索树的前序遍历
+	public void preOrder(){
+		preOrder(root);
+	}
+	
+	//前序遍历以node为根的二分搜索树，递归算法
+	private void preOrder(Node node){
+		if(node == null)
+			return;
+		
+		System.out.println(node.e);
+		preOrder(node.left);
+		preOrder(node.right);
+	}
+	
+	//二分搜索树的非递归前序遍历
+	public void preOrderNR(){
+		
+		Stack<Node> stack = new Stack<>();
+		stack.push(root);
+		while(!stack.isEmpty()){
+			Node cur = stack.pop();
+			System.out.println(cur.e);
+			
+			if(cur.right != null)
+				stack.push(cur.right);
+			if(cur.left != null)
+				stack.push(cur.left);
+		}
+	}
+	
+	//二分搜索树的中序遍历
+	public void inOrder(){
+		inOrder(root);
+	}
+	
+	//中序遍历以node为根的二分搜索树，递归算法
+	private void inOrder(Node node){
+		if(node == null)
+			return;
+		
+		inOrder(node.left);
+		System.out.println(node.e);
+		inOrder(node.right);
+	}
+	
+	//二分搜索树的后序遍历
+	public void postOrder(){
+		postOrder(root);
+	}
+	
+	//后序遍历以node为根的二分搜索树，递归算法
+	private void postOrder(Node node){
+		if(node == null)
+			return;
+		
+		postOrder(node.left);
+		postOrder(node.right);
+		System.out.println(node.e);
+	}
+	
+	//二分搜索树的层序遍历(广度优先遍历)
+	public void levelOrder(){
+		Queue<Node> q = new LinkedList<>();
+		q.add(root);
+		while(!q.isEmpty()){
+			Node cur = q.remove();
+			System.out.println(cur.e);
+			
+			if(cur.left != null)
+				q.add(cur.left);
+			if(cur.right != null)
+				q.add(cur.right);
+		}
+	}
+	
+	//寻找二分搜索树的最小元素
+	public E mininum(){
+		if(size == 0)
+			throw new IllegalArgumentException("BST is empty!");
+		
+		return mininum(root).e;
+	}
+	
+	//返回以node为根的二分搜索树的最小值所在的节点
+	private Node mininum(Node node){
+		if(node.left == null)
+			return node;
+		return mininum(node.left);
+	}
+	
+	//寻找二分搜索树的最大元素
+	public E maxinum(){
+		if(size == 0)
+			throw new IllegalArgumentException("BST is empty!");
+		
+		return maxinum(root).e;
+	}
+	
+	//返回以node为根的二分搜索树的最大值所在的节点
+	private Node maxinum(Node node){
+		if(node.right == null)
+			return node;
+		return maxinum(node.right);
+	}
+	
+	//从二分搜索树中删除最小值所在节点，返回最小值
+	public E removeMin(){
+		E ret = mininum();
+		root = removeMin(root);
+		return ret;
+	}
+	
+	//删除掉以node为根的二分搜索树种的最小节点
+	//返回删除节点后新的二分搜索树的根
+	private Node removeMin(Node node){
+		if(node.left == null){
+			Node rightNode = node.right;
+			node.right = null;
+			size --;
+			return rightNode;
+		}
+		
+		node.left = removeMin(node.left);
+		return node;
+	}
+	
+	//从二分搜索树中删除最大值所在节点，返回最大值
+	public E removeMax(){
+		E ret = maxinum();
+		root = removeMax(root);
+		return ret;
+	}
+	
+	//删除掉以node为根的二分搜索树种的最大节点
+	//返回删除节点后新的二分搜索树的根
+	private Node removeMax(Node node){
+		if(node.right == null){
+			Node leftNode = node.left;
+			node.left = null;
+			size --;
+			return leftNode;
+		}
+		
+		node.right = removeMax(node.right);
+		return node;
+	}
+	
+	//从二分搜索树中删除元素为e的节点
+	public void remove(E e){
+		root = remove(root,e);
+	}
+	
+	//删除以node为根的二分搜索树中值为e的节点，递归算法
+	//返回删除节点后新的二分搜索树的根
+	private Node remove(Node node, E e){
+		if(node == null)
+			return null;
+		
+		if(e.compareTo(node.e) < 0){
+			node.left = remove(node.left,e);
+			return node;
+		}else if(e.compareTo(node.e) > 0){
+			node.right = remove(node.right,e);
+			return node;
+		}else{ //e == node.e
+			
+			//待删除节点左子树为空的情况
+			if(node.left == null){
+				Node rightNode = node.right;
+				node.right = null;
+				size --;
+				return rightNode;
+			}
+			
+			//待删除节点右子树为空的情况
+			if(node.right == null){
+				Node leftNode = node.left;
+				node.left = null;
+				size --;
+				return leftNode;
+			}
+			
+			//待删除节点左右子树均不为空的情况
+			//找到比待删除节点大的最小节点，即待删除节点右子树的最小节点
+			//用这个节点顶替待删除节点的位置
+			Node successor = mininum(node.right);
+			successor.right = removeMin(node.right);
+			successor.left = node.left;
+			
+			node.left = node.right = null;
+			
+			return successor;
+		} 
+	}
+	
+	@Override
+	public String toString(){
+		StringBuilder res = new StringBuilder();
+		generateBSTString(root,0,res);
+		return res.toString();
+	}
+
+	//生成以node为根节点，深度为depth 的描述二叉树的字符串
+	private void generateBSTString(BinarySearchTree<E>.Node node, int depth, StringBuilder res) {
+
+		if(node == null){
+			res.append(generateDepthString(depth) + "null\n");
+			return;
+		}
+		
+		res.append(generateDepthString(depth) + node.e + "\n");
+		generateBSTString(node.left, depth + 1, res);
+		generateBSTString(node.right, depth + 1, res);
+	}
+
+	private String generateDepthString(int depth) {
+		StringBuilder res = new StringBuilder();
+		for(int i = 0; i < depth ; i++)
+			res.append("--");
+		return res.toString();
 	}
 }
